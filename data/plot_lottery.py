f894e6eea2f45c0bd78c23c742deca56e36f4f00
Tung Luu <luukhanhtung2002@gmail.com>
2022-10-08 14:36:49 -0400

    Add plot program

---
+++ data/plot_lottery.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def main():
+    data = pd.read_csv('lottery.csv')
+    plt.plot(list(range(0, 100)), data['10'].tolist(), label = '10 tickets')
+    plt.plot(list(range(0, 100)), data['20'].tolist(), label = '20 tickets')
+    plt.plot(list(range(0, 100)), data['30'].tolist(), label = '30 tickets')
+    plt.xlabel('Time')
+    plt.ylabel('Ticks')
+    plt.yticks(np.arange(0, 27, step = 1.5), np.arange(0, 27, step = 1.5))
+    plt.legend()
+    plt.show()
+
+main()
